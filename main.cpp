b8f2cbfeb5e2917184062e2887e97d703293df7c
Abulat <4518454+Abulat189@users.noreply.github.com>
2023-02-23 22:09:12 +0300

    initial setup

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+enum suits {
+  hearts = 0, //
+  diamonds = 1, //
+  clover = 2, //
+  spades = 3 //
+};
+
+struct card {
+  int i;
+};
+
+int main() {
+  return 0;
+}
